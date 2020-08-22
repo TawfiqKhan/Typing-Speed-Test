ed712232f0a978eed828eafdf780cd589a793e18
special3220 <special3220@yahoo.com>
2020-08-21 23:26:06 -0400

    Updates

---
+++ precache-manifest.6841b079751c46f1b92ef494a70c0123.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c256a11ad2c6fff42d7783016c46dfef",
+    "url": "/Typing-Speed-Test/index.html"
+  },
+  {
+    "revision": "069f505f318a79d1f23b",
+    "url": "/Typing-Speed-Test/static/css/main.56e97434.chunk.css"
+  },
+  {
+    "revision": "fc8dc91c00c13967d18f",
+    "url": "/Typing-Speed-Test/static/js/2.540466b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Typing-Speed-Test/static/js/2.540466b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "069f505f318a79d1f23b",
+    "url": "/Typing-Speed-Test/static/js/main.8a892011.chunk.js"
+  },
+  {
+    "revision": "b1f6500f2567f099b4e4",
+    "url": "/Typing-Speed-Test/static/js/runtime-main.6a58e7af.js"
+  }
+]);
